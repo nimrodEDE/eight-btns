f0a21643650602335d965afc5adafc012b76f452
nimrodEDE <nimni2233@gmail.com>
2026-02-14 01:31:03 +0200

    add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/eight-btns/",
   plugins: [react(), tailwindcss()],
 });
